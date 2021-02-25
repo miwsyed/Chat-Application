b2137e820f9058e6a5c41b3a9c4925db4054bd93
miwsyed <syed97mustafa@gmail.com>
2021-02-25 11:53:46 +0530

    getting firebase ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 // import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
